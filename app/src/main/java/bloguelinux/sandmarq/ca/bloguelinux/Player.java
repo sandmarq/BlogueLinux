17fe67fa0b95e6bd9659d1d6c132e357c41e3686
sandmarq <sandrine.marquis30@gmail.com>
2014-12-19 11:55:03 -0500

    Working 0.1.1

---
+++ app/src/main/java/bloguelinux/sandmarq/ca/bloguelinux/Player.java
@@ -0,0 +1,10 @@
+package bloguelinux.sandmarq.ca.bloguelinux;
+
+/**
+ * Created by SANDRINE.MARQUIS on 2014-12-19.
+ */
+public class Player {
+    public String url = "http://live.bloguelinux.ca/"; // your URL here
+
+
+}
