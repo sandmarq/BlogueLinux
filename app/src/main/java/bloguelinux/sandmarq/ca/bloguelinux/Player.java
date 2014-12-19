52e6c38b5c4eafd0f0c504916603ea60e2077c37
sandmarq <sandrine.marquis30@gmail.com>
2014-12-19 13:30:13 -0500

    Working 0.1.2 with class

--- app/src/main/java/bloguelinux/sandmarq/ca/bloguelinux/Player.java
+++ app/src/main/java/bloguelinux/sandmarq/ca/bloguelinux/Player.java
@@ -1,10 +1,79 @@
 package bloguelinux.sandmarq.ca.bloguelinux;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+
 /**
  * Created by SANDRINE.MARQUIS on 2014-12-19.
  */
 public class Player {
-    public String url = "http://live.bloguelinux.ca/"; // your URL here
+    private String url = null; // your URL here
+    private int status = 0; // 0 : Stop, 1 : opening, 2 : buffering, 3 : paused, 4 : Playing
+    private MediaPlayer mediaPlayer = new MediaPlayer();
 
+    public int getStatus() {
+        return status;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int Play() {
+        try {
+            mediaPlayer.setDataSource(url);
+            status = 1;
+        } catch (IOException e) {
+            Log.e("tag", e.getMessage(), e);
+            status = 0;
+        }
+        status = 2;
+        mediaPlayer.prepareAsync(); // might take long! (for buffering, etc)
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.start();
+                status = 4;
+            }
+
+        });
+        return status;
+    }
+
+    public int Stop() {
+        mediaPlayer.stop();
+        mediaPlayer.reset();
+        status = 3;
+        return status;
+    }
+
+    public int Pause() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            status = 3;
+        } else {
+            mediaPlayer.start();
+            status = 4;
+        }
+        return status;
+    }
+
+    public void Release() {
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+
+    public Player(String url) {
+        setUrl(url);
+    }
 
 }
