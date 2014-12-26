5020eb8ce547ddc052087a27ff39328777a16512
Sandrine Marquis <sandrine.marquis30@gmail.com>
2014-12-26 11:47:16 -0500

    ajout de vérificatio reseaux

--- app/src/main/java/bloguelinux/sandmarq/ca/bloguelinux/XmlParsingPod.java
+++ app/src/main/java/bloguelinux/sandmarq/ca/bloguelinux/XmlParsingPod.java
@@ -1,58 +1,34 @@
 package bloguelinux.sandmarq.ca.bloguelinux;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by SANDRINE.MARQUIS on 2014-12-21.
  */
 public class XmlParsingPod {
+
     private static final String TAG = "BlogueLinux|XMLParsing";
 
-    private String url = null; // your URL here
+    private String urlIn = null; // your URL here
     private String xmlContent = null;
 
     public String getUrl() {
-        return url;
+        return urlIn;
     }
 
     public void setUrl(String url) {
-        this.url = url;
+        urlIn = url;
     }
 
     public String getXmlFromUrl(String url) {
         String xml = null;
 
-        try {
-            // defaultHttpClient
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
-
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            xml = EntityUtils.toString(httpEntity);
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         // return XML
         return xml;
-    }
-
-    public String getXml() {
-        xmlContent = getXmlFromUrl(url);
-        return xmlContent;
     }
 
     public XmlParsingPod() {
